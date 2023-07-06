60aa4bf1a862bb6ff185ec209ea354f99e796b9a
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-07-06 10:23:03 -0700

    Keep it clean!

---
+++ 100-clean_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Deletes out-of-date archives, using the function do_clean
+"""
+from fabric.api import *
+
+
+env.hosts = ['54.157.182.28', '54.209.136.185']
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
