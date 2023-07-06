18b05bfe78b6838860935625a270e7c2e9298aab
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-07-06 08:53:32 -0700

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Generates a .tgz archive from the contents of web_static folder"""
+from fabric.api import local
+from time import strftime
+from datetime import date
+
+
+def do_pack():
+    """
+    Creates a tar gzipped archive from the web_static directory
+    """
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
